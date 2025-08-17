1efb925cda30b1f0a4ed440373cc1009a7d622b6
BuiBachNguyen <23521036@gm.uit.edu.vn>
2025-08-18 04:52:18 +0700

    NEW: Map Generator using scriptable object to config one by one for a level

---
+++ Assets/_Editor/MazeDataEditor.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MazeData))]
+public class MazeDataEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MazeData mazeData = (MazeData)target;
+
+        // Width & Length
+        mazeData.width = EditorGUILayout.IntField("Width", mazeData.width);
+        mazeData.length = EditorGUILayout.IntField("Length", mazeData.length);
+
+        // Auto resize nếu cần
+        if (mazeData.maze == null ||
+            mazeData.maze.Count != mazeData.length ||
+            (mazeData.maze.Count > 0 && mazeData.maze[0].row.Count != mazeData.width))
+        {
+            mazeData.Resize();
+        }
+
+        // 🎯 Camera
+        mazeData.camera = (GameObject)EditorGUILayout.ObjectField("Camera", mazeData.camera, typeof(GameObject), true);
+        mazeData.cameraRange = EditorGUILayout.FloatField("Camera Range", mazeData.cameraRange);
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Maze Layout", EditorStyles.boldLabel);
+
+        // Hiển thị dạng bảng
+        for (int y = 0; y < mazeData.length; y++)
+        {
+            EditorGUILayout.BeginHorizontal();
+            for (int x = 0; x < mazeData.width; x++)
+            {
+                mazeData.maze[y].row[x] = EditorGUILayout.IntField(
+                    mazeData.maze[y].row[x],
+                    GUILayout.Width(30)
+                );
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(mazeData);
+        }
+    }
+}
---
+++ Assets/_Scripts/Map/MapGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    #region Singleton
+    public static MapGenerator Instance;
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+    #endregion
+
+    [SerializeField] MazeData mazeData;
+    [SerializeField] Block[,] map;
+    [SerializeField] List<GameObject> obstaclesPrefabs;
+    public GameObject camera;
+
+    void Start()
+    {
+        // Gán Main Camera vào MazeData khi bắt đầu game
+        mazeData.camera = Camera.main.gameObject;
+
+        // Gọi hàm đặt vị trí camera
+        mazeData.SetCamPos();
+
+        // MAZE (1 = wall, 0 = lane)
+        List<MazeRow> maze = mazeData.maze;
+
+        map = new Block[mazeData.width, mazeData.length];
+
+        for (int z = 0; z < mazeData.length; z++)
+        {
+            for (int x = 0; x < mazeData.width; x++)
+            {
+                Vector3 pos = new Vector3(x, 1, z);
+
+                int cellValue = maze[z].row[x]; 
+
+                if (cellValue == 0)
+                {
+                    // Instantiate empty block prefab
+                    GameObject go = Instantiate(obstaclesPrefabs[0], pos, Quaternion.identity, this.transform);
+                    map[x, z] = go.GetComponent<EmptyBlock>();
+                }
+                else if (cellValue == 1)
+                {
+                    // Instantiate wall prefab
+                    GameObject go = Instantiate(obstaclesPrefabs[1], pos, Quaternion.identity, this.transform);
+                    map[x, z] = go.GetComponent<Wall>();
+                }
+            }
+        }
+    }
+}
